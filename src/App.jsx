35c763427b3be88e9a7b78def75164ef6d7ce95e
Rafael Schuster <rsc1335@gmail.com>
2021-05-28 13:40:36 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-ui/core';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">CLick me</Button>
+    </div>
+  );
+}
+
+export default App;
